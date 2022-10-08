c18482cf85f429117fbe4b0786f1fc345158b871
Lê Đức Mạnh <lemanh8456@gmail.com>
2022-10-08 08:38:37 +0700

    Initialize application

---
+++ CICD/src/test/java/CICDTEST/CICD/CicdApplicationTests.java
@@ -0,0 +1,13 @@
+package CICDTEST.CICD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
